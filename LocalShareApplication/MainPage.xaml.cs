c5769327a6335043e9d6e0a37ca0ba829ab05ea8
qWojtpl <wojtoo24@gmail.com>
2024-05-04 00:28:30 +0200

    First application files
    - Added first (initial) application files

---
+++ LocalShareApplication/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LocalShareApplication
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LocalShareApplication/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LocalShareApplication
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
