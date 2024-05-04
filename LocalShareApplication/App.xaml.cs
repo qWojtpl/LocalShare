ef02bcff1bff94a3191e1a58c7e89efef5d37b8d
qWojtpl <wojtoo24@gmail.com>
2024-05-04 16:23:03 +0200

    Pages
    - Added landing page styles
    - Added update page first styles
    - Added Sarabun font
    - Changed Windows window size
    - Added new images

--- LocalShareApplication/App.xaml.cs
+++ LocalShareApplication/App.xaml.cs
@@ -1,12 +1,31 @@
-﻿namespace LocalShareApplication
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿namespace LocalShareApplication;
 
-            MainPage = new MainPage();
-        }
+public partial class App : Application
+{
+
+    private readonly int width = 380;
+    private readonly int height = 660;
+
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
     }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        window.Width = width;
+        window.MinimumWidth = width;
+        window.MaximumWidth = width;
+        window.Height = height;
+        window.MinimumHeight = height;
+        window.MaximumHeight = height;
+        window.Title = "LocalShare";
+
+        return window;
+    }
+
 }
