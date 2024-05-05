c7960239e601473a36835b759f89881a7b5a5a84
qWojtpl <wojtoo24@gmail.com>
2024-05-05 14:02:18 +0200

    Settings manager
    - Added settings manager
    - Implemented first settings (port and callback port)
    - Added warning if callback port and port are the same

---
+++ LocalShareApplication/Misc/SettingsManager.cs
@@ -0,0 +1,61 @@
+﻿namespace LocalShareApplication.Misc;
+
+public static class SettingsManager
+{
+
+    public static int Port 
+    {
+        get {
+            return GetSetting("port", Default.Port);
+        }
+        set {
+            SetSetting("port", value);
+        }
+    }
+
+    public static int CallbackPort
+    {
+        get
+        {
+            return GetSetting("callbackPort", Default.CallbackPort);
+        }
+        set
+        {
+            SetSetting("callbackPort", value);
+        }
+    }
+
+    public class Default
+    {
+        public static int Port
+        {
+            get => 2780;
+        }
+
+        public static int CallbackPort
+        {
+            get => 2781;
+        }
+    }
+
+    private static int GetSetting(string key, int defaultValue)
+    {
+        return Preferences.Default.Get(key, defaultValue);
+    }
+
+    private static void SetSetting(string key, int value)
+    {
+        Preferences.Default.Set(key, value);
+    }
+
+    private static bool GetSetting(string key, bool defaultValue)
+    {
+        return Preferences.Default.Get(key, defaultValue);
+    }
+
+    private static void SetSetting(string key, bool value)
+    {
+        Preferences.Default.Set(key, value);
+    }
+
+}
