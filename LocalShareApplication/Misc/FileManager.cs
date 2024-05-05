6db2b8ef07202557fccb7eb1c28da9d8071f6a7b
qWojtpl <wojtoo24@gmail.com>
2024-05-05 14:10:22 +0200

    Added FileManager tests
    - Added FileManager tests
    - Bind upload button with FileManager

---
+++ LocalShareApplication/Misc/FileManager.cs
@@ -0,0 +1,23 @@
+﻿
+namespace LocalShareApplication.Misc;
+
+public static class FileManager
+{
+
+    public static void OpenFilePicker()
+    {
+        Task.Run(async () =>
+        {
+            IEnumerable<FileResult> results = await FilePicker.Default.PickMultipleAsync();
+            if(results == null)
+            {
+                return;
+            }
+            foreach(FileResult fileResult in results)
+            {
+                // code
+            }
+        });
+    }
+
+}
