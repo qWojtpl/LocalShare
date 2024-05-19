89fd66c0e5eba0728a795124d274a70848103694
qWojtpl <wojtoo24@gmail.com>
2024-05-19 17:39:40 +0200

    Fixed file size
    - Fixed file size
    - All files from removing packets commit

--- LocalShareCommunication/Packets/Packet.cs
+++ LocalShareCommunication/Packets/Packet.cs
@@ -9,8 +9,6 @@ public class Packet
     private PacketType _type;
     public string Key { get => _key; }
     private string _key;
-    public long Identifier { get => _identifier; }
-    private long _identifier = 0;
     public byte[] Data { get => _data; }
     private byte[] _data;
 
@@ -22,15 +20,13 @@ public class Packet
         }
         InitPacketType(responseData);
         InitKey(responseData);
-        InitIdentifier(responseData);
         InitData(responseData);
     }
 
-    public Packet(PacketType type, string key, long identifier, byte[] data)
+    public Packet(PacketType type, string key, byte[] data)
     {
         _type = type;
         _key = key;
-        _identifier = identifier;
         if(data.Length > Shared.MaxDataSize)
         {
             throw new Exception("Data too large.");
@@ -46,11 +42,6 @@ public class Packet
         for (int i = 0; i < Shared.KeyLength; i++)
         {
             newData[i + 1] = keyBytes[i];
-        }
-        byte[] identifierBytes = BitConverter.GetBytes(_identifier);
-        for (int i = 0; i < Shared.PacketIdentifierLength; i++)
-        {
-            newData[Shared.KeyLength + Shared.PacketTypeLength + i] = identifierBytes[i];
         }
         for (int i = 0; i < _data.Length; i++)
         {
@@ -84,16 +75,6 @@ public class Packet
             keyBytes[i] = responseData[i + Shared.PacketTypeLength];
         }
         _key = EncodingManager.GetText(keyBytes);
-    }
-
-    private void InitIdentifier(byte[] responseData)
-    {
-        byte[] identifierBytes = new byte[Shared.PacketIdentifierLength];
-        for (int i = 0; i < Shared.PacketIdentifierLength; i++)
-        {
-            identifierBytes[i] = responseData[i + Shared.KeyLength + Shared.PacketTypeLength];
-        }
-        _identifier = BitConverter.ToInt64(identifierBytes);
     }
 
     private void InitData(byte[] responseData)
--- LocalShareCommunication/Shared.cs
+++ LocalShareCommunication/Shared.cs
@@ -4,11 +4,10 @@ namespace LocalShareCommunication;
 public class Shared
 {
 
-    public static int KeyLength { get; } = 16;
+    public static int KeyLength { get; } = 8;
     public static int PacketTypeLength { get; } = 1;
-    public static int PacketIdentifierLength { get; } = 8;
     public static int MaxDataSize { get; set; } = 20000;
-    public static int HeaderLength { get => KeyLength + PacketTypeLength + PacketIdentifierLength; }
+    public static int HeaderLength { get => KeyLength + PacketTypeLength; }
     public static int PacketLength { get => HeaderLength + MaxDataSize; }
     public static int UploadTimeout { get; } = 30;
     public static string FilesPath { get; set; } = "./files/";
